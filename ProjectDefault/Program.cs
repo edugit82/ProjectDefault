c9b6def72dbc7cf392bb507a904c10297f45694e
Eduardo correa dos Santos <eduardocorrea82@hotmail.com>
2021-12-29 17:16:28 -0300

    Upload Files

---
+++ ProjectDefault/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+services.AddReact();
+
+// Make sure a JS engine is registered, or you will get an error!
+services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+  .AddV8();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Initialise ReactJS.NET. Must be before static files.
+app.UseReact(config =>
+{
+    // If you want to use server-side rendering of React components,
+    // add all the necessary JavaScript files here. This includes
+    // your components as well as all of their dependencies.
+    // See http://reactjs.net/ for more information. Example:
+    //config
+    //  .AddScript("~/js/First.jsx")
+    //  .AddScript("~/js/Second.jsx");
+
+    // If you use an external build too (for example, Babel, Webpack,
+    // Browserify or Gulp), you can improve performance by disabling
+    // ReactJS.NET's version of Babel and loading the pre-transpiled
+    // scripts. Example:
+    //config
+    //  .SetLoadBabel(false)
+    //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ ProjectDefault/wwwroot/js/components/TextBox.jsx
@@ -0,0 +1,18 @@
+﻿
+export class TextBox extends React.Component {
+
+    constructor() {
+        super()        
+    }
+
+    render() {
+        return (<input type="text" className="form-control" />);
+    }
+}
+
+let Create = function ()
+{
+    ReactDOM.render(<TextBox />, $("#indexbody")[0])
+}
+
+export { Create }
