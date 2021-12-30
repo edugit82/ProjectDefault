3d0f3c7f1ae38345cb90cca72d36645e04de2d48
Eduardo correa dos Santos <eduardocorrea82@hotmail.com>
2021-12-29 22:23:53 -0300

    Eleminina excessos

--- ProjectDefault/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectDefault.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
